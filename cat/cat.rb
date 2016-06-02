fb643da650e1f5ba2116745dba16ed2cb8393d8a
Ji You <kroyagis@gmail.com>
2016-06-02 13:25:20 -0400

    created eats_at method that returns the hour of the day with AM or PM that the cat eats

--- cat/cat.rb
+++ cat/cat.rb
@@ -1,7 +1,22 @@
 class Cat
+
   def initialize(name, preferred_food, meal_time)
     @name = name
     @preferred_food = preferred_food
     @meal_time = meal_time
+  end
+
+  def eats_at
+    if @meal_time == 0
+      return "12 AM"
+    elsif @meal_time == 12
+      return "12 PM"
+    elsif @meal_time < 12
+      return "#{@meal_time} AM"
+    elsif @meal_time > 12
+      return "#{@meal_time - 12} PM"
+    else
+      "Invalid hour"
+    end
   end
 end
