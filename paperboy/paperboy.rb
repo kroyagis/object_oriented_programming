5b73c90301b6f0756cb295c52feb6d7f50c67174
Ji You <kroyagis@gmail.com>
2016-06-02 15:17:30 -0400

    used .abs method to ensure the total number of house deliver always comes out to positive number

--- paperboy/paperboy.rb
+++ paperboy/paperboy.rb
@@ -14,35 +14,42 @@ class Paperboy
 
   def deliver(start_address, end_address)
 
-    total_house = (end_address - start_address) + 1
-
-    delivered = 0
-    if total_house % 2 == 0
-      delivered = total_house / 2
-    else
-      if start_address % 2 == 0
-        if @side == 'even'
-          delivered = (total_house / 2) + 1
-        elsif @side == 'odd'
-          delivered = (total_house / 2)
-        end
-      elsif start_address % 2 == 1
-        if @side == 'even'
-          delivered = (total_house / 2)
-        elsif @side == 'odd'
-          delivered = (total_house / 2) + 1
-        end
-      end
+    total_house = (end_address - start_address).abs + 1
+    #
+    # delivered = 0
+    # if total_house % 2 == 0
+    #   delivered = total_house / 2
+    # else
+    #   if start_address % 2 == 0
+    #     if @side == 'even'
+    #       delivered = (total_house / 2) + 1
+    #     elsif @side == 'odd'
+    #       delivered = (total_house / 2)
+    #     end
+    #   elsif start_address % 2 == 1
+    #     if @side == 'even'
+    #       delivered = (total_house / 2)
+    #     elsif @side == 'odd'
+    #       delivered = (total_house / 2) + 1
+    #     end
+    #   end
+    # end
+    # puts "delivered: #{delivered}"
+    if start_address < end_address
+      odd_house_number = (start_address..end_address).inject(0) { |sum, n| sum + (n % 2) }
+      even_house_number = total_house - odd_house_number
+    elsif start_address > end_address
+      odd_house_number = (start_address.downto(end_address)).inject(0) { |sum, n| sum + (n % 2) }
+      even_house_number = total_house - odd_house_number
     end
-    puts "delivered: #{delivered}"
 
-    # odd_house_number = (start_address..end_address).inject(0) { |sum, n| sum + (n % 2) }
-    # even_house_number = total_house - odd_house_number
-    # puts "total_house: #{total_house}"
-    # puts "odd_house_number: #{odd_house_number}"
-    # puts "even_house_number: #{even_house_number}"
+      puts "total_house: #{total_house}"
+      puts "odd_house_number: #{odd_house_number}"
+      puts "even_house_number: #{even_house_number}"
+
     # regular_pay = total_house * 0.25
     # over_time_pay = (total_house - @quota) * 0.50
+
     # if total_house <= @quota
     #   @earnings = regular_pay.to_f
     # elsif total_house > @quota
