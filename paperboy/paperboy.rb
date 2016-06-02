0995f49bb5871bf19a7a0dc386e369505a19d19e
Ji You <kroyagis@gmail.com>
2016-06-02 13:41:07 -0400

    created class Paperboy with name, quota, experience, side, earnings attributes

---
+++ paperboy/paperboy.rb
@@ -0,0 +1,11 @@
+class Paperboy
+
+  def initialize(name, quota, experience, side, earnings)
+    @name = name
+    @quota = quota
+    @experience = experience
+    @side = side
+    @earnings = earnings
+  end
+
+end
