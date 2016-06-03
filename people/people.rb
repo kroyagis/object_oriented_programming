f9d20bd8d776b9d8216f7b4442be9274a38bb7cc
Ji You <kroyagis@gmail.com>
2016-06-03 07:30:07 -0400

    created Person class that contains attribute name and an initializer to set the name

--- people/people.rb
+++ people/people.rb
@@ -1,11 +1,18 @@
+class Person
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+end
+
 class Student
   def learn
-    puts "I get it!"
+    puts 'I get it!'
   end
 end
 
 class Instructor
   def teach
-    puts "Evertyhing in Ruby is an Object"
+    puts 'Evertyhing in Ruby is an Object'
   end
 end
