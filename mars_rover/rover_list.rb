30bfd054dd7e39190fb956cf73ceed7c7da4bae3
Ji You <kroyagis@gmail.com>
2016-06-03 14:51:39 -0400

    created add_rover method to save Mars_rover instances in a @rovers array

--- mars_rover/rover_list.rb
+++ mars_rover/rover_list.rb
@@ -6,4 +6,13 @@ class Rover_list
   def initialize
     @rovers = []
   end
+
+  def add_rover(rover)
+    if rover.is_a?(Mars_rover)
+      @rovers << rover
+    else
+      raise "You must add an rover"
+    end
+  end
+
 end
